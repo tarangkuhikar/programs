d6771cf1915ec1119f9e3a265ae31868bf27f995
tarangkuhikar <tarangkuhikar@gmail.com>
2023-02-22 13:53:50 +0530

    addition file added

---
+++ HTML/addition.js
@@ -0,0 +1,17 @@
+var x=5;
+var y=6;
+
+var string1="hello";
+var string2=" all";
+
+var z=x+y;
+var a=x-y;
+var b=x*y;
+
+document.write("addition "+z+"<br>");
+document.write("subtraction "+a+"<br>");
+document.write("multiplication "+b+"<br>");
+document.write(string1+string2);
+document.write("<br>length of string "+string1.length);
+document.write("<br>character at 3: "+string1.charAt(1));
+document.write("<br>end: "+string1.endsWith("elo"));
