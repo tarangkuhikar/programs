92e62591b80529c0d2ebf1c5d817bebe8c02b9aa
tarangkuhikar <126047707+tarangkuhikar@users.noreply.github.com>
2023-02-24 11:09:18 +0530

    Add files via upload

---
+++ HTML/index.js
@@ -0,0 +1,67 @@
+
+// document.write("NAME : NISHIT CHAUDHARY<br>")
+// document.write("ENROLLMENTR : 2201030400013 <BR>")
+// document.write("DIV : A2")
+
+
+// x=100;
+// y=200
+
+// document.write(x+y)
+// document.write(x*y)
+// document.write(y/x)
+// document.write(x-y)
+
+ 
+
+// arr=[5]
+
+// arr[1]="THE ONE"
+// arr[3]=21
+// arr[2]=24
+// arr[0]=79 
+// arr[4]=24+24
+// document.write(arr[1])
+// document.write(arr[2])
+// window.document.write(arr[3])
+// window.document.write(arr[4])
+// window.document.write(arr[0])
+
+
+function cal(arg1,arg2)
+{
+    document.write(arg1+arg2)
+
+}
+document.write("<br>")
+cal(56,1)
+
+document.write("<br>")
+
+ 
+function fun(){
+    a=prompt("enter your name")
+    if(a==null || a ==""){
+        document.write("you denied for name")
+    }
+
+    else{
+        document.write("Hello"+a)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
